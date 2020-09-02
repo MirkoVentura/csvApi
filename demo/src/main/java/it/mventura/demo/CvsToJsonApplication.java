9b1267b7ea03bc9448e76d9e5bd9e4e71b9c8965
Mirko Ventura <mirko.ventura87@gmail.com>
2020-09-02 15:25:00 +0200

    first commit

---
+++ demo/src/main/java/it/mventura/demo/CvsToJsonApplication.java
@@ -0,0 +1,13 @@
+package it.mventura.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvsToJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvsToJsonApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/it/mventura/demo/CvsToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mventura.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvsToJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
