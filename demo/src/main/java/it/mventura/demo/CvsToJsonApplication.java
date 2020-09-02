ade91ee11df6c47d11013f20c73c9322d84b554d
Mirko Ventura <mirko.ventura87@gmail.com>
2020-09-02 18:21:03 +0200

    CommandLineRunner
    Sviluppato come command line runner, manca l’api rest

--- demo/src/main/java/it/mventura/demo/CvsToJsonApplication.java
+++ demo/src/main/java/it/mventura/demo/CvsToJsonApplication.java
@@ -1,7 +1,26 @@
 package it.mventura.demo;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 
 @SpringBootApplication
 public class CvsToJsonApplication {
@@ -10,4 +29,40 @@ public class CvsToJsonApplication {
 		SpringApplication.run(CvsToJsonApplication.class, args);
 	}
 
+	@Autowired
+	private Environment env;
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			//reading from config file.
+			String inputPath = env.getProperty("inputPath");
+			String outputPath = env.getProperty("outputPath");
+
+			//File f = new File(outputPath);
+			//f.createNewFile();
+
+			Path currentRelativePath = Paths.get("");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			System.out.println("Current relative path is: " + s);
+
+			System.out.println("Let's convert csv file from "+inputPath+" into json stored in "+outputPath);
+			//File input = ResourceUtils.getFile(s+"/"+inputPath);
+			File input = new File(inputPath);
+			File output = new File(outputPath);
+			try {
+				CsvSchema csv = CsvSchema.emptySchema().withHeader();
+				CsvMapper csvMapper = new CsvMapper();
+				MappingIterator<Map<?, ?>> mappingIterator =  csvMapper.reader().forType(Map.class).with(csv).readValues(input);
+				List<Map<?, ?>> data = mappingIterator.readAll();
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.writeValue(output, data);
+			} catch(Exception e) {
+				System.out.println("Exception catched "+e.getLocalizedMessage());
+				e.printStackTrace();
+			}
+
+
+		};
+	}
 }
